829d724972d69d5a104d341971933e9193224cba
ragavi25 <raghavimr15@gmail.cpm>
2020-02-12 18:27:57 +0530

    [Ragavi]add.implementation of program

---
+++ FactoryPattern/ComputerFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.Factory_Pattern
+{
+    public class ComputerFactory
+    {
+        public static Computer GetComputer(string type,string ram,string cpu,string hdd)
+        {
+            if ("PC".Equals(type))
+            {
+                return  new PC(ram, cpu, hdd);
+
+            }
+            else if("Server".Equals(type))
+            {
+                return new Server(ram, cpu, hdd);
+            }
+            
+            return null;
+            }
+
+        
+    }
+}
