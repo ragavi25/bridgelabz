add5e09d39d37ab1805336dc283e97bf1c36ff27
ragavi25 <raghavimr15@gmail.cpm>
2020-02-13 12:20:10 +0530

    [Ragavi]add.implementaion of program.

---
+++ ProtoTypeDesignPattern/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.ProtoTypeDesignPattern
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int DeptId { get; set; }
+        public override string ToString()
+        {
+            ////To print the employee Details.
+            return string.Format ("Name: {0} "+"DeptId : {1}",this.Name,this.DeptId.ToString());
+        }
+    }
+}
