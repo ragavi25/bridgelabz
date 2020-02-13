b867227739a604c11a25600b4e0ed8d959e28f1c
ragavi25 <raghavimr15@gmail.cpm>
2020-02-13 12:29:37 +0530

    [Ragavi]add.implementaion of program.

---
+++ ProtoTypeDesignPattern/Icloneable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.ProtoTypeDesignPattern
+{
+  public class Icloneable :Employee
+    {
+        /// <summary>
+        /// Icloneable interface one clone method return the object use the MemberwiseClone.
+        /// </summary>
+        /// <returns>Current object </returns>
+        public object Clone()
+        {
+            ////return the clone current object of Employee.
+            return this.MemberwiseClone();
+        }
+    }
+}
