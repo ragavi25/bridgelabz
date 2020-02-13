27eb5211095ea89d6b00ed80c22c81abaed9512d
ragavi25 <raghavimr15@gmail.cpm>
2020-02-13 09:46:59 +0530

    [Ragavi]Refactor.implementation of factorypattern programs.

--- FactoryPattern/Server.cs
+++ FactoryPattern/Server.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DesiginPattern.Factory_Pattern
+﻿namespace DesiginPattern.Factory_Pattern
 {
     /// <summary>
     /// 
