35cc9b6be9d62a85ea9fbf5e8306c74f588b00a4
ragavi25 <raghavimr15@gmail.cpm>
2020-02-13 09:46:26 +0530

    [Ragavi]Refactor.implementation of factorypattern programs.

--- FactoryPattern/Server.cs
+++ FactoryPattern/Server.cs
@@ -4,29 +4,38 @@ using System.Text;
 
 namespace DesiginPattern.Factory_Pattern
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Server : Computer
-    {
+    {/// <summary>
+     /// 
+     /// </summary>
         private readonly string cpu;
         private readonly string hdd;
         private readonly string ram;
 
         public Server(string ram, string cpu, string hdd)
         {
+            ////
             this.ram = ram;
             this.cpu = cpu;
             this.hdd = hdd;
         }
         public override string Ram()
         {
+            ////
             return this.ram;
         }
         public override string Cpu()
         {
+            ////
             return this.cpu;
         }
 
         public override string Hdd()
-        {
+        { 
+            ////
             return this.hdd;
         }
 
