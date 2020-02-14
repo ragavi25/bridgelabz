3e8bfc4bd355455f62b4a1f655885cb84c4a7b97
ragavi25 <raghavimr15@gmail.cpm>
2020-02-14 11:36:01 +0530

    [Ragavi]Add.Implemenation of program.

---
+++ StructuralDesignPatterns/Adapterdesignpattern/SocketAdapterImpl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.StructuralDesignPatterns.Adapterdesignpattern
+{
+    public class SocketAdapterImpl : Socket, ISocketAdapter
+    {
+        public Volt Get120Volt()
+        {
+            return GetVolt();
+        }
+
+        public Volt Get12Volt()
+        {
+            Volt v = GetVolt();
+            return convertVolt(v, 10);
+        }
+
+        private Volt convertVolt(Volt v, int v1)
+        {
+            return new Volt(v.getVolts() / v1);
+        }
+
+        public Volt Get3Volt()
+        {
+            Volt v = GetVolt();
+            return convertVolt(v, 40);
+        }
+    }
+}
