fe0e873ec76b1009c50f2b287a341b8a940ce57e
ragavi25 <raghavimr15@gmail.cpm>
2020-02-14 14:27:47 +0530

    [Ragavi]Refactor.AdapterPattern program.

---
+++ StructuralDesignPatterns/ProxyDesignPattern/CommandExecutor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.StructuralDesignPatterns.ProxyDesignPattern
+{
+    interface CommandExecutor
+    {
+
+    }
+}
