bf7f8229b91f15e835dd9c0b8a8e3ec1fa630ed0
ragavi25 <raghavimr15@gmail.cpm>
2020-02-14 11:21:46 +0530

    [Ragavi]Add.Implemenation of program.

---
+++ StructuralDesignPatterns/Adapterdesignpattern/SocketAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.StructuralDesignPatterns.Adapterdesignpattern
+{
+    public interface ISocketAdapter
+    {
+        public Volt Get120Volt();
+        public Volt Get12Volt();
+        public Volt Get3Volt(); 
+    }
+}
