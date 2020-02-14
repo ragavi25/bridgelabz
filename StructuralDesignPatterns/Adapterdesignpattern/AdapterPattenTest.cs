e771470d7bb3492f839ab29b8e4586bead08a17d
ragavi25 <raghavimr15@gmail.cpm>
2020-02-14 12:02:41 +0530

    [Ragavi]Add.Implemenation of program.

--- StructuralDesignPatterns/Adapterdesignpattern/AdapterPattenTest.cs
+++ StructuralDesignPatterns/Adapterdesignpattern/AdapterPattenTest.cs
@@ -4,7 +4,36 @@ using System.Text;
 
 namespace DesiginPattern.StructuralDesignPatterns.Adapterdesignpattern
 {
-    class AdapterPattenTest
+    public class AdapterPattenTest
     {
+        public void Test()
+        {
+
+        }
+        private static void TestObjectAdapter()
+        {
+            ISocketAdapter sockad = new SocketObjAdapterImpl();
+            {
+                Volt v3 = getVolt(sockad, 3);
+                Volt V12 = getVolt(sockad, 12);
+                Volt v120 = getVolt(sockad, 120);
+
+            }
+        }
+
+        private static Volt getVolt(ISocketAdapter sockad, int v)
+        {
+            switch(v)
+            {
+                case 3:
+                    return sockad.Get3Volt();
+                case 12:
+                    return sockad.Get12Volt();
+                case 120:
+                    return sockad.Get120Volt();
+                default:
+                    return sockad.Get120Volt();
+            }
+        }
     }
 }
