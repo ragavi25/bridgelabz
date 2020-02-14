cb39f23668159ac143ed0859bb0b874d444bedbf
ragavi25 <raghavimr15@gmail.cpm>
2020-02-14 12:45:05 +0530

    [Ragavi]Refactor.AdapterPattern program.

--- StructuralDesignPatterns/Adapterdesignpattern/ISocketAdapter.cs
+++ StructuralDesignPatterns/Adapterdesignpattern/ISocketAdapter.cs
@@ -5,7 +5,10 @@ using System.Text;
 namespace DesiginPattern.StructuralDesignPatterns.Adapterdesignpattern
 {
     public interface ISocketAdapter
-    {
+    {/// <summary>
+     /// purpose:create an adapter interface with these methods
+     /// </summary>
+     /// <returns></returns>
         public Volt Get120Volt();
         public Volt Get12Volt();
         public Volt Get3Volt(); 
