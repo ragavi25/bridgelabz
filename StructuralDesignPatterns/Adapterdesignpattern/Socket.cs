3f5c22fd8756838e3a2a3a5313948a17e9bde83d
ragavi25 <raghavimr15@gmail.cpm>
2020-02-14 12:38:42 +0530

    [Ragavi]Refactor.AdapterPattern program.

--- StructuralDesignPatterns/Adapterdesignpattern/Socket.cs
+++ StructuralDesignPatterns/Adapterdesignpattern/Socket.cs
@@ -6,6 +6,10 @@ namespace DesiginPattern.StructuralDesignPatterns.Adapterdesignpattern
 {
     public class Socket
     {
+        /// <summary>
+        /// Purpose: Socket class using producing constant volts of 120V
+        /// </summary>
+        /// <returns></returns>
         public Volt GetVolt()
         {
             return new Volt(120);
