0fda103983f93421e4768ee8a1fceeed7794f760
ragavi25 <raghavimr15@gmail.cpm>
2020-02-14 11:13:22 +0530

    [Ragavi]Add.Implemenation of program.

--- StructuralDesignPatterns/Adapterdesignpattern/Volt.cs
+++ StructuralDesignPatterns/Adapterdesignpattern/Volt.cs
@@ -6,5 +6,18 @@ namespace DesiginPattern.StructuralDesignPatterns.Adapterdesignpattern
 {
    public class Volt
     {
+        private int Volts;
+        public  Volt (int v)
+        {
+            this.Volts = v;
+        }
+        public int getVolts()
+        {
+            return Volts;
+        }
+        public void setVolts (int Volts)
+        {
+            this.Volts = Volts;
+        }
     }
 }
