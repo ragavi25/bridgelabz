7792e8bbb1ced5ded1c65f954ba025533c15580d
ragavi25 <raghavimr15@gmail.cpm>
2020-02-14 12:10:58 +0530

    [Ragavi]Add.Implemenation of program.

--- StructuralDesignPatterns/Adapterdesignpattern/AdapterPattenTest.cs
+++ StructuralDesignPatterns/Adapterdesignpattern/AdapterPattenTest.cs
@@ -8,7 +8,8 @@ namespace DesiginPattern.StructuralDesignPatterns.Adapterdesignpattern
     {
         public void Test()
         {
-
+            TestObjectAdapter();
+            TestClassAdapter();
         }
         private static void TestObjectAdapter()
         {
