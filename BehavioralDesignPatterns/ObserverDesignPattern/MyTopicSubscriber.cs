a268633c18895d571d84b5582954ddba94984368
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 18:40:12 +0530

    [Ragavi]Refactor.ObserverDesiginPattern programs.

--- BehavioralDesignPatterns/ObserverDesignPattern/MyTopicSubscriber.cs
+++ BehavioralDesignPatterns/ObserverDesignPattern/MyTopicSubscriber.cs
@@ -10,19 +10,32 @@ using System.Text;
 
 namespace DesiginPattern.BehavioralDesignPatterns.ObserverDesignPattern
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class MyTopicSubscriber : IObservers
     {
         private string Name;
         private ISubject topic;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="UserName"></param>
         public MyTopicSubscriber(string UserName)
         {
             this.Name = UserName;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subject"></param>
         public void setSubject(ISubject subject)
         {
             this.topic = subject;
         }
-
+        /// <summary>
+        /// /
+        /// </summary>
         public void Uptate()
         {
             string msg = (string)topic.GetUptate(this);
