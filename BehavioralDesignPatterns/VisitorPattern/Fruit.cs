7143ae2d2f2a0439980302bde06a355b7b379815
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 15:37:14 +0530

    [Ragavi]add.implementation of VisitorPattern program.

--- BehavioralDesignPatterns/VisitorPattern/Fruit.cs
+++ BehavioralDesignPatterns/VisitorPattern/Fruit.cs
@@ -1,6 +1,25 @@
 ﻿namespace DesiginPattern.BehavioralDesignPatterns.VisitorPattern
 {
-    public class Fruit
+    public class Fruit : ItemElement
     {
+        private string Name;
+        private int Weight;
+        private string Priceperkg;
+        public string getName()
+        {
+            return this.Name;
+        }
+        public int getWeight()
+        {
+            return Weight;
+        }
+        public string getPriceperkg()
+        {
+            return Priceperkg;
+        }
+        public int Accept(ShoppingCartVisitor visitor)
+        {
+            
+        }
     }
 }
