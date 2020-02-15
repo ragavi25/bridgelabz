1cee24e43ed799aa40117ebdc1b6d608d3955352
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 17:11:40 +0530

    [Ragavi]Add.implentation of MediatorDesignPattern.

---
+++ BehavioralDesignPatterns/MediatorDesignPattern/MediatorImpl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.BehavioralDesignPatterns.MediatorDesignPattern
+{
+    public class MediatorImpl : Mediator
+    {
+        private List<User> users;
+        public MediatorImpl ()
+        {
+            this.users = new List<User>();
+        }
+        public void AddUser(User user)
+        {
+            this.users.Add(user);
+        }
+
+        public void SendMessage(string msg, User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
