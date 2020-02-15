3b6b152856e50e41193424c4301751f03bfb5688
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 18:34:31 +0530

    [Ragavi]Refactor.MediatorDesiginPattern programs.

--- BehavioralDesignPatterns/MediatorDesignPattern/User.cs
+++ BehavioralDesignPatterns/MediatorDesignPattern/User.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=MediatorClient.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="R Ragavi"/>
+// -------------------------------------------------------------------------------------------------------
+using System;
 
 namespace DesiginPattern.BehavioralDesignPatterns.MediatorDesignPattern
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public abstract class User
     {
+        /// <summary>
+        /// 
+        /// </summary>
         protected Mediator M;
             protected string Name;
         public abstract void Send(string msg);
