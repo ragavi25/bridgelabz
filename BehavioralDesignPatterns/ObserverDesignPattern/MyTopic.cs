05ef4f1066422d2ffa6a7482ac044e2c2ff50ddd
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 11:23:58 +0530

    [Ragavi]Add.implentatin of ObserverDesignPattern

---
+++ BehavioralDesignPatterns/ObserverDesignPattern/MyTopic.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.BehavioralDesignPatterns.ObserverDesignPattern
+{
+   public class MyTopic
+    {
+    }
+}
