1c1cbd51a0f9fa744cfba52a2a3ae2c2ea01639c
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 16:56:07 +0530

    [Ragavi]Add.implentation of MediatorDesignPattern.

---
+++ BehavioralDesignPatterns/MediatorDesignPattern/Mediator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.BehavioralDesignPatterns.MediatorDesignPattern
+{
+    public interface Mediator
+    {
+    }
+}
