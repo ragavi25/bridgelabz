75bf6aa684c959e74534bba92ee12a1e7730d486
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 16:06:09 +0530

    [Ragavi]add.implementation of VisitorPattern program.

---
+++ BehavioralDesignPatterns/VisitorPattern/ShopingCardClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.BehavioralDesignPatterns.VisitorPattern
+{
+    public class ShopingCardClient
+    {
+        public static void Test()
+        {
+            ItemElement[] items = new ItemElement[]
+            {
+                new Book(50,"2754"),
+                new  Book(70,"2489"),
+                new Fruit("Apple","2kg",100),
+                new Fruit("Orange","1kg",80)
+            };
+            int Total = Calculate(items);
+            Console.WriteLine("Total Cost: " + Total);
+        }
+
+        private static int Calculate(ItemElement[] items)
+        {
+            int sum = 0;
+            ShoppingCartVisitor cartVisitor = new ShopingCardImpl();
+            foreach (ItemElement item in items)
+            {
+                
+            }
+            return sum;
+        }
+    }
+}
