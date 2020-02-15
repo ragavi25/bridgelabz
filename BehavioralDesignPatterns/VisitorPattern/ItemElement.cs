3fe48b9840fa5bda379dbf4dff21d5ba3b99ac20
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 19:08:51 +0530

    [Ragavi]Refactor.Update VisitorPattern program

--- BehavioralDesignPatterns/VisitorPattern/ItemElement.cs
+++ BehavioralDesignPatterns/VisitorPattern/ItemElement.cs
@@ -1,9 +1,18 @@
-﻿using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=ShoppingCardClient.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="R Ragavi"/>
+// -------------------------------------------------------------------------------------------------------
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DesiginPattern.BehavioralDesignPatterns.VisitorPattern
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface ItemElement
     {
         public int Accept(ShoppingCartVisitor visitor);
