bb71894ae003fac3bff872525f07fa5b12aebaec
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 15:29:48 +0530

    [Ragavi]add.implementation of VisitorPattern program.

--- BehavioralDesignPatterns/VisitorPattern/Book.cs
+++ BehavioralDesignPatterns/VisitorPattern/Book.cs
@@ -1,6 +1,20 @@
 ﻿namespace DesiginPattern.BehavioralDesignPatterns.VisitorPattern
 {
-    public class Book
+    public class Book : ItemElement
     {
+        private int Cost;
+        private string ISBNumber;
+       public int getCost()
+        {
+            return Cost;
+        }
+        public string getISBNumber()
+        {
+            return ISBNumber;
+        }
+        public int Accept(ShoppingCartVisitor visitor)
+        {
+            return visitor.Visit(this);
+        }
     }
 }
