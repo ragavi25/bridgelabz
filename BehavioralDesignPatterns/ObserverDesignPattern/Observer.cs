9b5eddc6be0abcb5fe436864648f10304a6a26b9
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 18:54:17 +0530

    [Ragavi]Refactor.ObserverDesiginPattern programs.

--- BehavioralDesignPatterns/ObserverDesignPattern/Observer.cs
+++ BehavioralDesignPatterns/ObserverDesignPattern/Observer.cs
@@ -1,7 +1,16 @@
-﻿using DesiginPattern.BehavioralDesignPatterns.ObserverDesignPattern;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=ObserverPatternTest.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="R Ragavi"/>
+// -------------------------------------------------------------------------------------------------------
+using DesiginPattern.BehavioralDesignPatterns.ObserverDesignPattern;
 
 namespace DesiginPattern.BehavioralDesignPatterns
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IObservers
     {
         public void Uptate();
