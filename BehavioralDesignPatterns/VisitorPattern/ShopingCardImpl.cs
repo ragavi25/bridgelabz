24c5d06971da4757b291f73c0ee55a657df15d96
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 15:48:55 +0530

    [Ragavi]add.implementation of VisitorPattern program.

---
+++ BehavioralDesignPatterns/VisitorPattern/ShopingCardImpl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesiginPattern.BehavioralDesignPatterns.VisitorPattern
+{
+    public class ShopingCardImpl : ShoppingCartVisitor
+    {
+        public int Visit(Book book)
+        {
+            int price = 0;
+            if(book.getCost() > 100)
+            {
+                price = book.getCost() - 10;
+            }
+            else
+            {
+                price = book.getCost();
+            }
+            Console.WriteLine("BOOK ISBN:" + book.getISBNumber() + "Price:" + price);
+            return price;
+
+
+        }
+
+        public int Visit(Fruit fruit)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
