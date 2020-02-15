682057be721022d6206008f05b75f6052317d539
ragavi25 <raghavimr15@gmail.cpm>
2020-02-15 19:08:02 +0530

    [Ragavi]Refactor.Update VisitorPattern program

--- BehavioralDesignPatterns/VisitorPattern/Book.cs
+++ BehavioralDesignPatterns/VisitorPattern/Book.cs
@@ -1,26 +1,51 @@
-﻿namespace DesiginPattern.BehavioralDesignPatterns.VisitorPattern
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=ShoppingCardClient.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="R Ragavi"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesiginPattern.BehavioralDesignPatterns.VisitorPattern
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Book : ItemElement
     {
         private readonly int Cost;
         private readonly string ISBNumber;
         private readonly int v1;
         private readonly string v2;
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Cost"></param>
+        /// <param name="ISBNumber"></param>
         public Book(int Cost, string ISBNumber)
         {
             this.Cost = Cost;
             this.ISBNumber= ISBNumber;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public int getCost()
         {
             return Cost;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public string getISBNumber()
         {
             return ISBNumber;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="visitor"></param>
+        /// <returns></returns>
         public int Accept(ShoppingCartVisitor visitor)
         {
             return visitor.Visit(this);
--- BehavioralDesignPatterns/VisitorPattern/Fruit.cs
+++ BehavioralDesignPatterns/VisitorPattern/Fruit.cs
@@ -1,4 +1,10 @@
-﻿namespace DesiginPattern.BehavioralDesignPatterns.VisitorPattern
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=ShoppingCardClient.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="R Ragavi"/>
+// -------------------------------------------------------------------------------------------------------
+namespace DesiginPattern.BehavioralDesignPatterns.VisitorPattern
 {
     public class Fruit : ItemElement
     {
@@ -6,9 +12,12 @@
         private int Weight;
         private int Priceperkg;
         private string v1;
-        private string v2;
-        private int v3;
-        private int v;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <param name="Weight"></param>
+        /// <param name="Priceperkg"></param>
 
         
         public Fruit(string Name, int Weight, int Priceperkg)
@@ -17,19 +26,35 @@
             this.Weight = Weight;
             this.Priceperkg = Priceperkg;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public string getName()
         {
             return this.Name;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public int getWeight()
         {
             return Weight;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public int getPriceperkg()
         {
             return Priceperkg;
         }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="visitor"></param>
+        /// <returns></returns>
         public int Accept(ShoppingCartVisitor visitor)
         {
             return visitor.Visit(this);
