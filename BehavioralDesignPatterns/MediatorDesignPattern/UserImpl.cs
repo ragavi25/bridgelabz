d12abf8f691a99d7f389bad19c88643857464aab
ragavi25 <raghavimr15@gmail.cpm>
2020-02-17 09:37:30 +0530

    [Ragavi]Refactor.update MediatorDesignPattern program.

--- BehavioralDesignPatterns/MediatorDesignPattern/UserImpl.cs
+++ BehavioralDesignPatterns/MediatorDesignPattern/UserImpl.cs
@@ -11,26 +11,21 @@ using System.Text;
 namespace DesiginPattern.BehavioralDesignPatterns.MediatorDesignPattern
 {
     /// <summary>
-    /// 
+    /// purpose: we can create concrete User classes to be used by client.
     /// </summary>
     public class UserImpl : User
     {
         /// <summary>
-        /// 
+        /// this oly purpose of readonly User Name.
         /// </summary>
-        private readonly string Name;
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="m"></param>
-        /// <param name="Name"></param>
+       
         public UserImpl(Mediator m, string Name)
         {
             M = m;
             this.Name = Name;
         }
         /// <summary>
-        /// 
+        /// purpose:Recevie() method used to Receive message to users.
         /// </summary>
         /// <param name="msg"></param>
 
@@ -39,7 +34,7 @@ namespace DesiginPattern.BehavioralDesignPatterns.MediatorDesignPattern
             Console.WriteLine(this.Name + "Recivied Message: " +msg);
         }
         /// <summary>
-        /// 
+        /// Purpose:send() method is using mediator to send the message to the users and it has no idea how it will be handled by the mediator.
         /// </summary>
         /// <param name="msg"></param>
         public override void Send(string msg)
