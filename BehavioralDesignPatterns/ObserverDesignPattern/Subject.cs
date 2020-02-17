279a1e6805943c1a239fbb0ce67d797d24ffa503
ragavi25 <raghavimr15@gmail.cpm>
2020-02-17 10:05:29 +0530

    [Ragavi]Refactor.update ObserverDesignPattern program.

--- BehavioralDesignPatterns/ObserverDesignPattern/Subject.cs
+++ BehavioralDesignPatterns/ObserverDesignPattern/Subject.cs
@@ -10,13 +10,29 @@ using System.Text;
 namespace DesiginPattern.BehavioralDesignPatterns.ObserverDesignPattern
 {
     /// <summary>
-    /// 
+    /// Creating Subject Interface used to communication to Observers.
     /// </summary>
      public interface ISubject
     {
+        /// <summary>
+        /// Method to Register Observere.
+        /// </summary>
+        /// <param name="obj"></param>
         public void Register(IObservers obj);
+        /// <summary>
+        /// Method to Unregister Observer.
+        /// </summary>
+        /// <param name="obj"></param>
         public void UnRegister(IObservers obj);
+        /// <summary>
+        /// method to notify observers of change
+        /// </summary>
         public void NotifyObserver();
+        /// <summary>
+        /// Method to getUpdate from Observer.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
         public Object GetUptate(IObservers obj);
     }
 }
