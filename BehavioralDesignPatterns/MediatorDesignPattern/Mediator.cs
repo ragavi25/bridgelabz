c19a20d991f0d7807576d28030a6ae1106fe1b62
ragavi25 <raghavimr15@gmail.cpm>
2020-02-17 09:22:21 +0530

    [Ragavi]Refactor.update MediatorDesignPattern program.

--- BehavioralDesignPatterns/MediatorDesignPattern/Mediator.cs
+++ BehavioralDesignPatterns/MediatorDesignPattern/Mediator.cs
@@ -3,7 +3,7 @@
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
 // <creator name="R Ragavi"/>
-// ----------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -11,17 +11,17 @@ using System.Text;
 namespace DesiginPattern.BehavioralDesignPatterns.MediatorDesignPattern
 {
     /// <summary>
-    /// 
+    /// Purpose : create Mediator interface that will define the contract for concrete mediators.
     /// </summary>
     public interface Mediator
     {/// <summary>
-    /// 
+    /// create a method used to sendMessage to users.
     /// </summary>
     /// <param name="msg"></param>
     /// <param name="user"></param>
            public void SendMessage(String msg, User user);
         /// <summary>
-        /// 
+        /// create a method used to AddUsers.
         /// </summary>
         /// <param name="user"></param>
             void AddUser(User user);
