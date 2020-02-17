724b91f60853e67640c053d7b3c2de64c7a3ac5e
ragavi25 <raghavimr15@gmail.cpm>
2020-02-17 09:54:09 +0530

    [Ragavi]Refactor.update ObserverDesignPattern program.

--- BehavioralDesignPatterns/ObserverDesignPattern/MyTopicSubscriber.cs
+++ BehavioralDesignPatterns/ObserverDesignPattern/MyTopicSubscriber.cs
@@ -11,14 +11,14 @@ using System.Text;
 namespace DesiginPattern.BehavioralDesignPatterns.ObserverDesignPattern
 {
     /// <summary>
-    /// 
+    /// purpose:Here is the implementation of Observers that will watch over the subject.
     /// </summary>
     public class MyTopicSubscriber : IObservers
     {
         private string Name;
         private ISubject topic;
         /// <summary>
-        /// 
+        /// purpose:Creating MyTopicSubscriber used to Current UserName.
         /// </summary>
         /// <param name="UserName"></param>
         public MyTopicSubscriber(string UserName)
@@ -26,7 +26,7 @@ namespace DesiginPattern.BehavioralDesignPatterns.ObserverDesignPattern
             this.Name = UserName;
         }
         /// <summary>
-        /// 
+        /// SetSubject used to set the current topic.
         /// </summary>
         /// <param name="subject"></param>
         public void setSubject(ISubject subject)
@@ -34,7 +34,7 @@ namespace DesiginPattern.BehavioralDesignPatterns.ObserverDesignPattern
             this.topic = subject;
         }
         /// <summary>
-        /// /
+        ///  implementation of update() method where it’s calling Subject getUpdate() method to get the message to consume. We could have avoided this call by passing message as argument to update() method.
         /// </summary>
         public void Uptate()
         {
