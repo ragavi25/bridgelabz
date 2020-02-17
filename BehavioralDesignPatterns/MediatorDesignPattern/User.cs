e67b640618883bc826a31c9095763919ce355105
ragavi25 <raghavimr15@gmail.cpm>
2020-02-17 09:32:37 +0530

    [Ragavi]Refactor.update MediatorDesignPattern program.

--- BehavioralDesignPatterns/MediatorDesignPattern/User.cs
+++ BehavioralDesignPatterns/MediatorDesignPattern/User.cs
@@ -9,13 +9,11 @@ using System;
 namespace DesiginPattern.BehavioralDesignPatterns.MediatorDesignPattern
 {
     /// <summary>
-    /// 
+    /// Purpose:Users can send and receive messages, so we can have User interface or abstract class. I am creating User as abstract class.
     /// </summary>
     public abstract class User
     {
-        /// <summary>
-        /// 
-        /// </summary>
+       
         protected Mediator M;
             protected string Name;
         public abstract void Send(string msg);
