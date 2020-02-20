b5fdfda608dac8c17c0165c7413424f984bf7f3c
ragavi25 <raghavimr15@gmail.cpm>
2020-02-20 19:02:42 +0530

    [Ragavi]ADD.implementation of  the EmployeeManagement

---
+++ Context/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Model;
+using Microsoft.EntityFrameworkCore;
+using EmployeeManagement;
+
+namespace EmployeeManagement
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext>options):base(options)
+        {
+
+        }
+        /// <summary>
+        /// Initial Property
+        /// 
+        /// </summary>
+        public DbSet<Employees> employees { get; set; }
+
+    }
+}
