f126343f1db2f04e913eedc21188ab390d1d1dc3
ragavi25 <raghavimr15@gmail.cpm>
2020-02-21 19:14:23 +0530

    [Ragavi]Add.Employee viwes

---
+++ EmployeeView/EmpView.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeManagement.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.EmployeeView
+{
+  public interface EmpView
+    {
+       bool Login(Employees employees);
+        void Register(Employees employees);
+        void Update(Employees employees);
+        void getemployee(Employees employees);
+        
+    }
+}
