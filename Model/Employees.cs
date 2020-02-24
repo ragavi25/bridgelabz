8203542903f0f2b190d84926e56bbb9b8c9704af
ragavi25 <raghavimr15@gmail.cpm>
2020-02-24 19:09:24 +0530

    [Ragavi]Refactor.updated Employee Model.

--- Model/Employees.cs
+++ Model/Employees.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using EmployeeManagement;
@@ -8,30 +9,17 @@ namespace EmployeeManagement.Model
 {
     public class Employees
     {
-        [Required]
-        private int UserId;
-        [Required]
+        private int ID;
         private string firstName;
-        [Required]
         private string lastName;
-        [Required]
         private string email;
-        [Required]
-        private string userName;
-        [Required]
         private string password;
-        [Required]
         private string mobile;
-        [Required]
-        private string address;
-
-        public int Id { get => this.UserId; set =>this.UserId = value; }
         public string FirstName { get => this.firstName; set =>this.firstName = value; }
         public string LastName { get => this.lastName; set => this.lastName = value; }
         public string Email { get => this.email; set =>this. email = value; }
-        public string UserName { get =>this. userName; set => this.userName = value; }
         public string Password { get =>this. password; set =>this. password = value; }
         public string Mobile { get =>this. mobile; set =>this. mobile = value; }
-        public string Address { get => this.address; set =>this. address = value; }
+        public int ID1 { get => ID; set => ID = value; }
     }
 }
