76a3ba32e782a789e372a92d71aa9f9988afb80d
ragavi25 <raghavimr15@gmail.cpm>
2020-02-28 19:14:11 +0530

    [Ragavi]Add.Implementation of Manager.

---
+++ Manager/AccountInterface/IAccountManager.cs
@@ -0,0 +1,13 @@
+﻿using Fundoo.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Manager
+{
+    public interface IAccountManger
+    {
+        public Task<bool> Register(RegisterModel registerModel);
+    }
+}
---
+++ Manager/Manager/AccountManagerImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Fundoo.Model;
+using Repository.Repository;
+
+namespace Manager.Manager
+{
+   public class AccountManagerImpl :IAccountManger
+    {
+        
+        private IAccountRep Repository;
+        public AccountManagerImpl(IAccountRep repository)
+        {
+            this.Repository = repository;
+        }
+        public async Task<bool> Register(RegisterModel registerModel)
+        {
+            await this.Repository.CreateRegister(registerModel);
+            return true;
+            
+        }
+    }
+}
+ 
