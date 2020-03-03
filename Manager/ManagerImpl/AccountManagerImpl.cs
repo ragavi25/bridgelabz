d078236f40bda9e81f177ffc5c9d11e706a31447
ragavi25 <raghavimr15@gmail.cpm>
2020-03-03 19:23:46 +0530

    [Ragavi]Refactor.implementation of Manager.

---
+++ Manager/ManagerImpl/AccountManagerImpl.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Fundoo.Model;
+using Model.Model;
+using Repository.Repository;
+
+namespace Manager.Manager
+{
+   public class AccountManagerImpl :IAccountManger
+    {
+        
+        private IAccountRep Repository;
+
+        public AccountManagerImpl()
+        {
+        }
+
+        public AccountManagerImpl(IAccountRep repository)
+        {
+            this.Repository = repository;
+        }
+
+        public Task<RegisterModel> EmailLogin(LoginModel loginModel)
+        {
+            return this.Repository.EmailLogin(loginModel);
+        }
+
+        public Task<RegisterModel> FaceBookLogin(LoginModel loginModel)
+        {
+            return this.Repository.FaceBookLogin(loginModel);
+        }
+
+        public Task<string> ForgotPassword(ForgotPasswordModel forgotPassword)
+        {
+            return this.Repository.ForgotPassword(forgotPassword);
+        }
+
+        public string Login(LoginModel loginModel)
+        {
+            return this.Repository.Login(loginModel);
+        }
+
+        public async Task<bool> Register(RegisterModel registerModel)
+        {
+            await this.Repository.CreateRegister(registerModel);
+            return true;
+            
+        }
+
+        public async Task<string> ResetPassWord(ResetPassWord reset)
+        {
+           string a= await this.Repository.ResetPassWord(reset);
+            return a ;
+        }
+    }
+}
+ 
---
+++ Manager/ManagerInterface/IAccountManager.cs
@@ -0,0 +1,19 @@
+﻿using Fundoo.Model;
+using Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Manager
+{
+    public interface IAccountManger
+    {
+        public Task<bool> Register(RegisterModel registerModel);
+        public string Login(LoginModel loginModel);
+        public Task<string> ForgotPassword(ForgotPasswordModel forgotPassword);
+        public Task<string> ResetPassWord(ResetPassWord reset);
+        public Task<RegisterModel> EmailLogin(LoginModel loginModel);
+        public Task<RegisterModel> FaceBookLogin(LoginModel loginModel);
+    }
+}
