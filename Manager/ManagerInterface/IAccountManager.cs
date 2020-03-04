6bd41772a378e121115a692fa6251ee7805571ef
ragavi25 <raghavimr15@gmail.cpm>
2020-03-04 12:33:52 +0530

    [Ragavi]Refactor.Update the Manager social login.

--- Manager/ManagerInterface/IAccountManager.cs
+++ Manager/ManagerInterface/IAccountManager.cs
@@ -1,19 +1,55 @@
-﻿using Fundoo.Model;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=AccountController.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="R Ragavi"/>
+// --------------------------------------------------------------------------------------------------------------------
+using Fundoo.Model;
 using Model.Model;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Manager.Manager
 {
+    /// <summary>
+    /// Purpose:get the all Social login purpose.
+    /// </summary>
     public interface IAccountManger
     {
+        /// <summary>
+        /// Purpose:Get the Register .
+        /// </summary>
+        /// <param name="registerModel"></param>
+        /// <returns></returns>
         public Task<bool> Register(RegisterModel registerModel);
+        /// <summary>
+        /// purpose:get the User login.
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         public string Login(LoginModel loginModel);
+        /// <summary>
+        /// purpose:get the user  Forgot Password.
+        /// </summary>
+        /// <param name="forgotPassword"></param>
+        /// <returns></returns>
         public Task<string> ForgotPassword(ForgotPasswordModel forgotPassword);
+        /// <summary>
+        /// purpose:get the user ResetPassword.
+        /// </summary>
+        /// <param name="reset"></param>
+        /// <returns></returns>
         public Task<string> ResetPassWord(ResetPassWord reset);
+        /// <summary>
+        /// Purpose:Get the User EmailLogin.
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         public Task<RegisterModel> EmailLogin(LoginModel loginModel);
+        /// <summary>
+        /// purpose:Get the User FacebookLogin.
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         public Task<RegisterModel> FaceBookLogin(LoginModel loginModel);
     }
 }
