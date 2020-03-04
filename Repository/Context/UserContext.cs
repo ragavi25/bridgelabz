0504a1c896b0668ca25b55a62a6cb79354a35862
ragavi25 <raghavimr15@gmail.cpm>
2020-03-04 12:41:05 +0530

    [Ragavi].Refactor.Update the dbcontext.

--- Repository/Context/UserContext.cs
+++ Repository/Context/UserContext.cs
@@ -1,18 +1,27 @@
-﻿using Fundoo.Model;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=AccountController.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="R Ragavi"/>
+// --------------------------------------------------------------------------------------------------------------------
+using Fundoo.Model;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace Repository.Context
 {
-   public class UserContext : DbContext
+    public class UserContext : DbContext
    {
+        /// <summary>
+        /// Purpose:UseContext using the Database connection.
+        /// </summary>
+        /// <param name="options"></param>
         public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
 
         }
-       
+       /// <summary>
+       /// Purpose:create the Registers table.
+       /// </summary>
         public DbSet<RegisterModel> registers { get; set; }
       
 
