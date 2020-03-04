a1949e90cd8cf8b525b9ebc0dbe5cf1f53a4bc72
ragavi25 <raghavimr15@gmail.cpm>
2020-03-04 12:33:00 +0530

    [Ragavi]Refactor.Update the Manager social login.

--- Manager/ManagerImpl/AccountManagerImpl.cs
+++ Manager/ManagerImpl/AccountManagerImpl.cs
@@ -1,6 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=AccountController.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="R Ragavi"/>
+// --------------------------------------------------------------------------------------------------------------------
 using System.Threading.Tasks;
 using Fundoo.Model;
 using Model.Model;
@@ -8,47 +11,77 @@ using Repository.Repository;
 
 namespace Manager.Manager
 {
-   public class AccountManagerImpl :IAccountManger
+    public class AccountManagerImpl :IAccountManger
     {
-        
-        private IAccountRep Repository;
-
+        //// AccountManager get the Repository.  
+        private readonly IAccountRep Repository;
+        /// <summary>
+        /// purpose:Using TestCase
+        /// </summary>
         public AccountManagerImpl()
         {
         }
 
+        /// <summary>
+        /// Purpose:passing the Repository.
+        /// </summary>
+        /// <param name="repository"></param>
         public AccountManagerImpl(IAccountRep repository)
         {
             this.Repository = repository;
         }
-
+        /// <summary>
+        /// Purpose:Get the Repository EmailLogin.
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         public Task<RegisterModel> EmailLogin(LoginModel loginModel)
         {
             return this.Repository.EmailLogin(loginModel);
         }
-
+        /// <summary>
+        /// Purpose:Get the Repository FacebookLogin.
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         public Task<RegisterModel> FaceBookLogin(LoginModel loginModel)
         {
             return this.Repository.FaceBookLogin(loginModel);
         }
-
+        /// <summary>
+        /// Purpose:Get the Repository ForgotPassword.
+        /// </summary>
+        /// <param name="forgotPassword"></param>
+        /// <returns></returns>
         public Task<string> ForgotPassword(ForgotPasswordModel forgotPassword)
         {
             return this.Repository.ForgotPassword(forgotPassword);
         }
-
+        /// <summary>
+        /// Purpose:Get the Repository Login.
+        /// </summary>
+        /// <param name="loginModel"></param>
+        /// <returns></returns>
         public string Login(LoginModel loginModel)
         {
-            return this.Repository.Login(loginModel);
+            return  this.Repository.Login(loginModel);
         }
-
+        /// <summary>
+        /// Purpose:Get the Repository Register.
+        /// </summary>
+        /// <param name="registerModel"></param>
+        /// <returns></returns>
         public async Task<bool> Register(RegisterModel registerModel)
         {
             await this.Repository.CreateRegister(registerModel);
             return true;
             
         }
-
+        /// <summary>
+        /// Purpose:Get the Repository ResetPassword.
+        /// </summary>
+        /// <param name="reset"></param>
+        /// <returns></returns>
         public async Task<string> ResetPassWord(ResetPassWord reset)
         {
            string a= await this.Repository.ResetPassWord(reset);
