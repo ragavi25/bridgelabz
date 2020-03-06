7ffa739cd45b15fd567bf4eb477d3ac647e5ddb4
ragavi25 <raghavimr15@gmail.cpm>
2020-03-06 19:11:50 +0530

    [Ragavi]Refactor.Update Migration.

---
+++ Repository/Migrations/20200305132249_new.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Repository.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "labels",
+                columns: table => new
+                {
+                    LabelId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    LabelName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_labels", x => x.LabelId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Nodes",
+                columns: table => new
+                {
+                    NodeId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Tittle = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Createdate1 = table.Column<DateTime>(nullable: true),
+                    Modifieddate = table.Column<DateTime>(nullable: true),
+                    Archive = table.Column<bool>(nullable: false),
+                    Pin = table.Column<bool>(nullable: false),
+                    ChangeColor = table.Column<string>(nullable: true),
+                    AddImg = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Nodes", x => x.NodeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "registers",
+                columns: table => new
+                {
+                    FirstName = table.Column<string>(nullable: false),
+                    LastName = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Password = table.Column<string>(nullable: false),
+                    Id = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_registers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "labels");
+
+            migrationBuilder.DropTable(
+                name: "Nodes");
+
+            migrationBuilder.DropTable(
+                name: "registers");
+        }
+    }
+}
