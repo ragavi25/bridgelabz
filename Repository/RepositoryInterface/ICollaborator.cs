605d112b9e92e56f198cfca3a5c12bd68a8cba0c
ragavi25 <raghavimr15@gmail.cpm>
2020-03-09 16:40:58 +0530

    [Ragavi]Refactor.update Repository.

--- Repository/RepositoryInterface/ICollaborator.cs
+++ Repository/RepositoryInterface/ICollaborator.cs
@@ -1,14 +1,45 @@
-﻿using Model.Model.Collaborators;
-using System;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=CollaboratorController.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="R Ragavi"/>
+// --------------------------------------------------------------------------------------------------------------------
+using Model.Model.Collaborators;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Repository.RepositoryInterface
 {
-   public  interface ICollaborator
-   {
+    public  interface ICollaborator
+    {
+        /// <summary>
+        /// Purpose:Create the AddCollaborator.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="email"></param>
+        /// <param name="senderemail"></param>
+        /// <param name="receiveemail"></param>
+        /// <returns></returns>
         public Task<int> AddCollaborator(int id,string email,string senderemail,string receiveemail);
-   }
+        /// <summary>
+        /// Purpose:Create the GetCollaborator.
+        /// </summary>
+        /// <returns></returns>
+        public List<CollaboratorModel> GetCollaborators();
+        /// <summary>
+        /// Purpose:Create the Delete.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Task<int> Delete(int id);
+        /// <summary>
+        /// Purpose:Create the Update.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="email"></param>
+        /// <param name="Receivedemail"></param>
+        /// <returns></returns>
+        //public Task<int> Update(int id, string email,string Receivedemail);
+    }
 }
 
