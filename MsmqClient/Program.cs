c153d53dfa9cd5d8be08ed37bf560c1871aa19cc
ragavi25 <raghavimr15@gmail.cpm>
2020-03-11 14:31:15 +0530

    [Ragavi]Add.MSMQClient programs.

---
+++ MsmqClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using Experimental.System.Messaging;
+using System;
+
+namespace MsmqClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Latest Message");
+            MessageQueue Myqueue;
+            Myqueue = new MessageQueue(@".\private$\Myqueue");
+            Message message = Myqueue.Receive();
+            message.Formatter = new BinaryMessageFormatter();
+            Console.WriteLine(message.Body.ToString());
+            Console.ReadLine();
+
+        }
+    }
+}
