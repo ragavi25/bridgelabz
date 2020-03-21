5379ff549ab2ab56f0301d76b6f1f0d254ae9ef6
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-03-21 13:13:04 +0530

    [Ragavi]Add.implementation of module.

---
+++ src/app/app.module.ts
@@ -0,0 +1,59 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatMenuModule } from '@angular/material/menu';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule, Router } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { ForgotpasswordComponent } from './component/forgotpassword/forgotpassword.component';
+import { HomeComponent } from './component/home/home.component';
+import { AccountService } from './Services/account.service';
+import { CommonModule } from '@angular/common';
+import { ServiceCardComponent } from './component/service-card/service-card.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotpasswordComponent,
+    HomeComponent,
+    ServiceCardComponent
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    MatInputModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatIconModule,
+    FormsModule,
+    RouterModule,
+    AppRoutingModule,
+   RouterModule.forRoot([])
+    
+    
+  ],
+  providers: [AccountService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
