94b91843c9c84874cfecfb69c2f548ab06e50a94
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-04 19:53:33 +0530

    [Ragavi]Add.SpecFlow steps

---
+++ Steps/LoginSteps.cs
@@ -0,0 +1,45 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo.Steps
+{
+    [Binding]
+  public class LoginSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+        
+        [Given(@"I have navigated to my application")]
+        public void GivenIhaveNavigatedToMyApplication()
+        {
+            // Browser.Current.Navigate().GoToUrl(ConfigurationManager.AppSettings["seleniumBaseUrl"]);
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/login");
+        }
+
+        [Given(@"I type the raghavimr(.*)@gmail\.com and (.*)")]
+        public void GivenITypeTheRaghavimrGmail_ComAnd(string p, string R)
+        {
+            LoginPage loginpage = new LoginPage(currentDriver);
+            ///ScenarioContext.Current.Pending();
+            loginpage.Login(p, R);
+        }
+     
+        [When(@"I click login button")]
+        public void WhenIClickLoginButton()
+        {
+            LoginPage loginpage = new LoginPage(currentDriver);
+            PropertyCollection.CurrentPage = loginpage.Clicklogin();
+        }
+        [Then(@"I should see the Dashborad page")]
+        public void ThenIShouldSeeTheDashboradPage()
+        {
+
+        }
+    }
+}
