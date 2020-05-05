d0898a50637033521545fcf294a57befc0b42686
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-05 16:01:16 +0530

    [Ragavi]Add.Logout page

---
+++ Pages/LogoutPage.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace seleniumFundoo.Pages
+{
+    class LogoutPage :BasePage
+    {
+        IWebDriver driver;
+        
+        public LogoutPage(IWebDriver currentDriver)
+        {
+            PageFactory.InitElements(currentDriver, this);
+            this.driver = currentDriver;
+        }
+        [FindsBy(How = How.Id, Using = "btnlogout")]
+        private IWebElement btnlogout;
+
+        public LoginPage ClickSignout()
+        {
+            btnlogout.Click();
+            return new LoginPage(driver);
+        }
+
+
+    }
+}
