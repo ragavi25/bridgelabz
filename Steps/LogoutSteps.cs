be8173bc43600a900254ddef9d7d03c25e31a795
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-05 16:02:22 +0530

    [Ragavi]Add.Logout steps

---
+++ Steps/LogoutSteps.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo
+{
+    [Binding]
+    public class LogoutSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+
+        [Given(@"I have navigated to Dashborad Application")]
+        public void GivenIHaveNavigatedToDashboradApplication()
+        {
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/fundoo");
+            currentDriver.Manage().Window.FullScreen();
+        }
+        
+        [When(@"I click signout button")]
+        public void WhenIClickSignoutButton()
+        {
+            LogoutPage logout = new LogoutPage(currentDriver);
+            PropertyCollection.CurrentPage = logout.ClickSignout();
+        }
+    }
+}
