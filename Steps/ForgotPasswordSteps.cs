cd60e985cc94e8f415ec65e09acc27f3c1b4d96b
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-05 14:40:03 +0530

    [Ragavi]Add.ForgotPassWord Steps

---
+++ Steps/ForgotPasswordSteps.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo
+{
+    [Binding]
+    public class ForgotPasswordSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+
+        [Given(@"I have navigated to ForgotPassword application")]
+        public void GivenIHaveNavigatedToForgotPasswordApplication()
+        {
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/forgot");
+        }
+        
+        [Given(@"I type the (.*)")]
+        public void GivenITypeThe(string p0)
+        {
+            ForgotPage forgot = new ForgotPage(currentDriver);
+            forgot.Forgot(p0);
+        }
+        
+        [When(@"I click Forgot button")]
+        public void WhenIClickForgotButton()
+        {
+            ForgotPage forgot = new ForgotPage(currentDriver);
+            PropertyCollection.CurrentPage = forgot.ClickForgot();
+        }
+    }
+}
