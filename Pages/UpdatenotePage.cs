07dfa04bbe937cd602f03acb93602d0958486698
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-07 15:49:23 +0530

    [Ragavi]Add.UpdateNote page

---
+++ Pages/UpdatenotePage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace seleniumFundoo.Pages
+{
+    class UpdatenotePage
+    {
+        IWebDriver driver;
+
+        public UpdatenotePage(IWebDriver currentDriver)
+        {
+            PageFactory.InitElements(currentDriver, this);
+            this.driver = currentDriver;
+
+        }
+        [FindsBy(How = How.Id, Using = "ntitle")]
+        private IWebElement txtTitle;
+
+        [FindsBy(How = How.Id, Using = "titles")]
+        private IWebElement txtDescription;
+
+        [FindsBy(How = How.Id, Using = "updatesave")]
+        private IWebElement btnClose;
+
+        public void Updatenote(string Title, string Description)
+        {
+            System.Threading.Thread.Sleep(5000);
+            txtTitle.SendKeys(Title);
+            txtDescription.SendKeys(Description);
+        }
+        public DashboradPage ClickClose()
+        {
+            btnClose.Click();
+            return new DashboradPage(driver);
+        }
+    }
+}
