07f12bf8dc6f94d319468215c328ad4cb56b958a
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-07 15:51:49 +0530

    [Ragavi]Add.Updatenote steps

---
+++ Steps/UpdatenoteSteps.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo
+{
+    [Binding]
+    public class UpdatenoteSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+
+        [Given(@"I have navigated to displaynote Application")]
+        public void GivenIHaveNavigatedToDisplaynoteApplication()
+        {
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/fundoo/getnote?page=notes&view=row");
+            currentDriver.Manage().Window.Maximize();
+        }
+        [Given(@"I have update the (.*) and (.*)")]
+        public void GivenIHaveUpdateTheAnd(string p0, string p1)
+        {
+            UpdatenotePage updatenotePage = new UpdatenotePage(currentDriver);
+            updatenotePage.Updatenote(p0, p1);
+        }
+
+        [When(@"I click updateClose button")]
+        public void WhenIClickUpdateCloseButton()
+        {
+            UpdatenotePage updatenotePage = new UpdatenotePage(currentDriver);
+            PropertyCollection.CurrentPage = updatenotePage.ClickClose();
+        }
+    }
+}
