21460a2c6c6ddf2c96a0aec1b32c5e889304192e
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:19:54 +0530

    [Ragavi]Add.Achive

---
+++ Pages/Archive.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace seleniumFundoo.Pages
+{
+    class Archive
+    {
+        IWebDriver driver;
+
+        public Archive(IWebDriver currentDriver)
+        {
+            PageFactory.InitElements(currentDriver, this);
+            this.driver = currentDriver;
+        }
+        [FindsBy(How = How.Id, Using = "TxtTakeNote")]
+        private IWebElement txtCard;
+
+        [FindsBy(How = How.Id, Using = "archive")]
+        private IWebElement btnicon;
+
+        public DashboradPage clickarchive()
+        {
+            txtCard.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+            btnicon.Click();
+                return new DashboradPage(driver);
+        }
+    }
+}
