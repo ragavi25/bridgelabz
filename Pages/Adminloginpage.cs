d84f52d3a5fbc87839cba6d3e5223bd541521418
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:18:35 +0530

    [Ragavi]Add.Adminpage

---
+++ Pages/Adminloginpage.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace seleniumFundoo.Pages
+{
+    class Adminloginpage
+    {
+        IWebDriver driver;
+        public Adminloginpage(IWebDriver currentDriver)
+        {
+            PageFactory.InitElements(currentDriver, this);
+            this.driver = currentDriver;
+        }
+        [FindsBy(How = How.Id, Using = "mat-input-0")]
+        private IWebElement txtEmail;
+
+        [FindsBy(How = How.Id, Using = "mat-input-1")]
+        private IWebElement txtPassword;
+
+        [FindsBy(How = How.ClassName, Using = "mat-raised-button")]
+        private IWebElement btnLogin;
+
+        public void AdminLogin(string Email, string Password)
+        {
+            System.Threading.Thread.Sleep(5000);
+            txtEmail.SendKeys(Email);
+            txtPassword.SendKeys(Password);
+
+        }
+        public void Clicklogin()
+        {
+            btnLogin.Submit();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(4);
+            driver.Quit();
+            //return new DashboradPage(driver);
+        }
+    }
+}
+
