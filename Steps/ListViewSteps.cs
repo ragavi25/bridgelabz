d1c3083ac2be92875ed8f8a3ec398d48e383c3d5
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:53:41 +0530

    [Ragavi]Add.ListViewSteps

---
+++ Steps/ListViewSteps.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo
+{
+    [Binding]
+    public class ListViewSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+        [Given(@"I have navigate Dashborad page")]
+        public void GivenIHaveNavigateDashboradPage()
+        {
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/fundoo/getnote?page=notes&view=row");
+            currentDriver.Manage().Window.Maximize();
+        }
+        
+        [When(@"I click the listview")]
+        public void WhenIClickTheListview()
+        {
+            ListViewPage listView = new ListViewPage(currentDriver);
+            PropertyCollection.CurrentPage = listView.ClickView();
+        }
+    }
+}
