d96692e4a16d31c2811ee59af578116fa4783f15
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:51:35 +0530

    [Ragavi]Add.DeleteNote

---
+++ Steps/DeleteNoteSteps.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo
+{
+    [Binding]
+    public class DeleteNoteSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+        [Given(@"I have a navigate to display page")]
+        public void GivenIHaveANavigateToDisplayPage()
+        {
+            currentDriver.Manage().Window.Maximize();
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/fundoo");
+        }
+        [Given(@"I have click the more icons")]
+        public void GivenIHaveClickTheMoreIcons()
+        {
+            DeleteNotePage deleteNote = new DeleteNotePage(currentDriver);
+            deleteNote.more();
+        }
+
+        [When(@"I have click DeleteNote")]
+        public void WhenIHaveClickDeleteNote()
+        {
+            DeleteNotePage deleteNote = new DeleteNotePage(currentDriver);
+            deleteNote.DeleteNote();
+        }
+    }
+}
