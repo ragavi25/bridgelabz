3f8cedeb0e7750e8243405953389b1e184c81bae
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:42:03 +0530

    [Ragavi]Add.PinPage

---
+++ Pages/PinPage.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace seleniumFundoo.Pages
+{
+    class PinPage
+    {
+        IWebDriver driver;
+
+        public PinPage(IWebDriver currentDriver)
+        {
+            PageFactory.InitElements(currentDriver, this);
+            this.driver = currentDriver;
+        }
+
+        [FindsBy(How = How.Id, Using = "TxtTakeNote")]
+        private IWebElement txtCard;
+
+        [FindsBy(How = How.Id, Using = "takepin")]
+        private IWebElement btnicon;
+
+        [FindsBy(How = How.Id, Using = "takeunpin")]
+        private IWebElement btnunpin;
+
+
+        public void clickPin()
+        {
+            txtCard.Click();
+            System.Threading.Thread.Sleep(2000);
+            btnicon.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+            btnunpin.Click();
+            //return new DashboradPage(driver);
+        }
+    }
+}
