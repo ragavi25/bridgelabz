6e408fb1269a275d52ddbe003be55c346f27cd1e
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:54:14 +0530

    [Ragavi]Add.PinSteps

---
+++ Steps/PinSteps.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo
+{
+    [Binding]
+    public class PinSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+        [Given(@"I have see Dashborad page")]
+        public void GivenIHaveSeeDashboradPage()
+        {
+            currentDriver.Manage().Window.Maximize();
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/fundoo");
+
+        }
+        [When(@"I click the pin")]
+        public void WhenIClickThePin()
+        {
+            PinPage pin = new PinPage(currentDriver);
+            pin.clickPin();
+        }
+    }
+}
