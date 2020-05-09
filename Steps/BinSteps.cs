e643787a7588c2d0ddd66976057704e01a2c9ae7
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:48:23 +0530

    [Ragavi]Add.BinSteps

---
+++ Steps/BinSteps.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo
+{
+    [Binding]
+    public class BinSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+        [Given(@"I have navigate a Dashborad page")]
+        public void GivenIHaveNavigateADashboradPage()
+        {
+            currentDriver.Manage().Window.Maximize();
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/fundoo");
+        }
+        [Given(@"I have click the Bin")]
+        public void GivenIHaveClickTheBin()
+        {
+            BinPage bin = new BinPage(currentDriver);
+            bin.clickbin();
+        }
+        
+        [When(@"I have click the Deleteall")]
+        public void WhenIHaveClickTheDeleteall()
+        {
+            BinPage bin = new BinPage(currentDriver);
+            bin.delete();
+            //currentDriver.Quit();
+        }
+        
+        [Then(@"I have navigate Bin")]
+        public void ThenIHaveNavigateBin()
+        {
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/fundoo/Bin?page=Bin&view=row");
+        }
+        
+        [Then(@"I have click the Restore")]
+        public void ThenIHaveClickTheRestore()
+        {
+            BinPage bin = new BinPage(currentDriver);
+            bin.restor();
+        }
+    }
+}
