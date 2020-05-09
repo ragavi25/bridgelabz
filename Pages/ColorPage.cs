1d52d18a9a8e25c4085dbe16a7c448f3a361554a
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:38:27 +0530

    [Ragavi]Add.color

---
+++ Pages/ColorPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace seleniumFundoo.Pages
+{
+    class ColorPage
+    {
+        IWebDriver driver;
+
+        public ColorPage(IWebDriver currentDriver)
+        {
+            PageFactory.InitElements(currentDriver, this);
+            this.driver = currentDriver;
+        }
+        [FindsBy(How = How.Id, Using = "TxtTakeNote")]
+        private IWebElement txtCard;
+
+        [FindsBy(How = How.Id, Using = "color")]
+        private IWebElement btnicon;
+
+        [FindsBy(How = How.XPath, Using = "//div[@class='ng-star-inserted']//div[1]//div[1]//button[1]")]
+        private IWebElement changecolor;
+
+        public void clickicon()
+        {
+            txtCard.Click();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+            btnicon.Click();
+        }
+        public void colors()
+        {
+            //System.Threading.Thread.Sleep(5000);
+            changecolor.Click();
+        }
+
+        public DashboradPage checkcolor()
+        {
+            changecolor.Click();
+            return new DashboradPage(driver);
+        }
+    }
+}
