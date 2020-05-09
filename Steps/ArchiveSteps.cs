de0750684fac50fcfb52e6ba6896bf35c89cbfb8
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:47:43 +0530

    [Ragavi]Add.Archive steps

---
+++ Steps/ArchiveSteps.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo
+{
+    [Binding]
+    public class ArchiveSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+        [Given(@"I m see the Dashborad page")]
+        public void GivenIMSeeTheDashboradPage()
+        {
+            currentDriver.Manage().Window.Maximize();
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/fundoo");
+        }
+
+        [When(@"I click the Archive")]
+        public void WhenIClickTheArchive()
+        {
+            Archive archive = new Archive(currentDriver);
+            PropertyCollection.CurrentPage = archive.clickarchive();
+            currentDriver.Quit();
+        }
+    }
+}
