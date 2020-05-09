75c32d0a9808eb856f7b9e2988c760e1f5512e5c
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-09 16:45:23 +0530

    [Ragavi]Add.AdminloginSteps

---
+++ Steps/AdminloginSteps.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using seleniumFundoo.Pages;
+using System;
+using TechTalk.SpecFlow;
+
+namespace seleniumFundoo
+{
+    [Binding]
+    public class AdminloginSteps
+    {
+        IWebDriver currentDriver = new ChromeDriver();
+        [Given(@"I have navigated to Adminlogin application")]
+        public void GivenIHaveNavigatedToAdminloginApplication()
+        {
+            currentDriver.Manage().Window.Maximize();
+            currentDriver.Navigate().GoToUrl("http://localhost:4200/Adminlogin");
+
+        }
+        [Given(@"I have take data")]
+        public void GivenIHaveTakeData(Table table)
+        {
+           // ScenarioContext.Current.Pending();
+        }
+
+        [When(@"I click Adminlogin button")]
+        public void WhenIClickAdminloginButton()
+        {
+            Adminloginpage adminloginpage = new Adminloginpage(currentDriver);
+            adminloginpage.Clicklogin();
+        }
+    }
+}
