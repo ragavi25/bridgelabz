b2df6fdfd454816a9f88b4783cf88699dc84693a
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-05-15 18:41:46 +0530

    [Ragavi]Refactor.note

--- src/app/component/getnote/getnote.component.ts
+++ src/app/component/getnote/getnote.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { NoteService } from 'src/app/Services/note.service';
 import { Route } from '@angular/compiler/src/core';
-import { MatSnackBar } from '@angular/material';
+import { MatSnackBar, MatDialog } from '@angular/material';
 import { Subject } from 'rxjs';
 import { FormControl } from '@angular/forms';
+import { CollaboratorComponent } from '../collaborator/collaborator.component';
+import { ImageuploadComponent } from '../imageupload/imageupload.component';
+import { EditlabelComponent } from '../editlabel/editlabel.component';
 
 @Component({
   selector: 'app-getnote',
@@ -11,71 +14,96 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./getnote.component.scss']
 })
 export class GetnoteComponent implements OnInit {
-  reminderData: any = "";
-  setColor = '#FFFFF';
-  isArchive: any = false;
+  //reminder= '5/9/2020';
+  //setColor = '#FFFFF';
+  archive: any = false;
   value: boolean = false;
-  constructor(private serviceobj:NoteService,private bar:MatSnackBar) { }
+  allNotes: any;
+  @Input() reminder: any;
+  @Input() labelname:any;
+  time = "8:00 AM";
+  repeat = "daily";
+  day = "Today";
+  notes: any;
+  constructor(private serviceobj:NoteService,private bar:MatSnackBar,public dialog: MatDialog) { }
   @Output() childEvent = new EventEmitter<any>();
-  parentSubject: Subject<any> = new Subject();
   flag = true;
-  bgColor = '#FFFFF';
+  bgColor = 'pink';
   message: string;
+  changeColor:string;
 
-  noteTitle = new FormControl('',
+  title = new FormControl('',
   );
-  noteContent = new FormControl('',
+  description = new FormControl('',
   );
   ngOnInit() {
+    this.getNotes();
 
+  }
+  getNotes(){
+    this.serviceobj.Getnote().subscribe((status :any)=>{
+      this.allNotes=status;
+    });
   }
   receiveMessage($event) {
     this.message = $event;
     this.bgColor = $event.color;
   }
-  receiveReminderEvent($event) {
-    console.log("event received for reminder: ", $event)
-    this.reminderData = $event;
-    console.log(this.reminderData)
+  ReminderEvent($event) {
+    console.log("event received for reminder: ",$event);
+    this.reminder = $event;
+    console.log(this.reminder);
   }
-  receiveSetColorEvent($event) {
+  reciveSetColorEvent($event) {
+    debugger;
+    $event='rgb(255, 179, 255)';
     console.log("change color event received");
     this.setColor = $event;
     console.log(this.setColor);
   }
 
-  receiveArchiveEvent($event) {
-    this.isArchive = $event;
+  ArchiveEvent(event) {
+    this.archive = event;
+    console.log(this.archive);
     this.bar.open("archived", "", { duration: 1000 })
   }
   reverseFlag() {
     this.flag = !this.flag;
   }
-
+  addRemainder(){
+    this.serviceobj.Reminder(this.notes.id, "Today, 8:00 PM").subscribe((status)=>{
+     if (status != null){
+       //this.sharing.upmessage(true);
+       this.bar.open('Remainder added.','', {duration: 2000});
+     }
+     //this.output.emit("done");
+       });
+      }
   removeReminder() {
-    this.reminderData = "";
+    this.reminder= "";
   }
   addNote() {
+    debugger;
     this.flag=!this.flag;
     try{
-      if (this.noteTitle.value == '' || this.noteContent.value == '') { throw new Error('Note must contain a title and content'); }
+      if (this.title.value == '' || this.description.value == '') { throw new Error('Note must contain a title and content'); }
 
       const newNoteData = {
-        userId: localStorage.getItem('userId'),
-        noteTitle: this.noteTitle.value,
-        noteContent: this.noteContent.value,
-        reminder: this.reminderData,
-        color: this.setColor,
-        isPined: this.value,
-        isArchived: this.isArchive.isArchived,
-        isDeleted: false,
-        img: ""
+       // nodeId: localStorage.getItem('nodeId'),
+        title: this.title.value,
+        description: this.description.value,
+        archive: this.archive.archive,
+        pin: this.value,
+        changeColor: this.setColor,
+        addImg: "",
+        reminder: this.reminder,
+        bin: false,
+        labelname:this.labelname
       };
-      this.serviceobj.AddNote(newNoteData,'AddNote').subscribe(
+      this.serviceobj.AddNote(newNoteData).subscribe(
         data => {
-          var pushData = (data as any).response.result;
-          this.parentSubject.next(pushData);
-
+          console.log(" Note",data);
+       console.log(data);
           this.bar.open('Your note has been saved successfully', '', { duration: 2000 });
         },
         error => {
@@ -87,6 +115,105 @@ export class GetnoteComponent implements OnInit {
       this.bar.open(arr,'',{duration:2000});
     }
   }
+  reverseValue() {
+    this.value = !this.value;
+  }
+  setColor(Color) {
+    this.changeColor = Color;
+  }
+  save(date) {
+    let str: any;
+    if (date != "") {
+      let v = new Date(date);
+      str = v.toDateString();
+    }
+    else str = this.day;
+    this.reminder = str + " " + this.time;
+  }
+  collaborator(){
+    const display=this.dialog.open(CollaboratorComponent,{
+       data:this.notes
+    });
+    display.afterClosed().subscribe(result=>{
+      console.log('closed');
+    });
+  }
+  delete(id: number) {
+    this.serviceobj.emptybin(id).subscribe(response => {
+      console.log(response);
+      //this.output.emit("done");
+    })
+}
+  addLabel(labels) {
 
+    console.log(labels);
+   // this.value.noteid = labels;
+    const dialogRef = this.dialog.open(EditlabelComponent, {
+      data: { labels:labels },
+      //panelClass: 'custom-dialog-container'
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      labels = result;
+     // this.output.emit("done");
+    });
+  }
+ 
+  addImage(){
+    var id:1;
+     const dialogR=this.dialog.open(ImageuploadComponent,{
+       width:'450px',
+       height:'200px',data:id
+     });
+ 
+   }
+
+  close() {
+    this.reminder = null;
+  }
+
+  closelabel() {
+    this.labelname = null;
+  }
+
+  listOfColors = [
+    [
+      { color: "rgb(255, 179, 255)", name: "pink" },
+      { color: "rgb(255, 255, 128)", name: "darkGolden" },
+      { color: "rgb(200, 232, 104)", name: "yellow" },
+      { color: " rgb(158, 136, 191)", name: "darkYellow" }
+    ],
+    [
+      { color: "slategray", name: "grey" },
+      { color: "rgb(153, 221, 255)", name: "Teal" },
+      { color: "rgb(203,240,248)", name: "blue" },
+      { color: "rgb(174,203,250)", name: "Dark blue" },
+    ],
+    [
+      { color: "rgb(255, 153, 0)", name: "orange" },
+      { color: "rgb(97, 191, 82)", name: "green" },
+       { color: "white", name: "white" },
+      { color: " rgb(196,174,251)", name: "purpule" }
+  
+    ]
+  
+  ]
+
+  isArchive() { 
+    if (this.archive == 0) {
+      this.archive = 1;
+      this.bar.open('Note Archived', 'Dismiss', { duration: 2000 });
+    }
+    else {
+      this.archive = 0;
+      this.bar.open('Note UnArchived', 'Dismiss', { duration: 2000 });
+    }
+  }
+  setTime(value) {
+    this.time = value;
+  }
+
+  setRepitation(value) {
+    this.repeat = value;
+  }
 
 }
