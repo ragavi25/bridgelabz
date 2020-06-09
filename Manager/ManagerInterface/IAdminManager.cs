9f1dd3ae445b2d29a9ef7b99c1bd41c3b0fe6fcf
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-06-09 15:16:47 +0530

    Add files via upload

---
+++ Manager/ManagerInterface/IAdminManager.cs
@@ -0,0 +1,16 @@
+﻿using Model.Model;
+using Model.Model.Admin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.ManagerInterface
+{
+   public interface IAdminManager
+    {
+        Task<int> AddAdmin(Admins admins);
+        string LoginAdmin(Adminlogin admins);
+        List<Adminlist> GetList();
+    }
+}
