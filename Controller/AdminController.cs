b5cd4fa89e58d4253e58d70ba7ffb0ed9856b7ac
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-06-09 15:15:33 +0530

    [Ragavi]Refactor.Admin

---
+++ Controller/AdminController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Manager.ManagerInterface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Model;
+using Model.Model.Admin;
+
+namespace Fundoo.Controller
+{
+   
+    public class AdminController : ControllerBase
+    {
+        private readonly IAdminManager adminManager;
+        public AdminController(IAdminManager manager)
+        {
+            adminManager = manager;
+        }
+
+        [HttpPost]
+        [Route("addAdmin")]
+        public async Task<ActionResult> AddAdmin([FromBody] Admins admins)
+        {
+            try
+            {
+                var result = await this.adminManager.AddAdmin(admins);
+                return Ok(result);
+            }
+            catch(Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpPost]
+        [Route("loginAdmin")]
+        public ActionResult LoginAdmin([FromBody] Adminlogin admins)
+        {
+            try { 
+            var result = this.adminManager.LoginAdmin(admins);
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        [HttpGet]
+        [Route("getList")]
+        public IEnumerable<Adminlist> GetUserList()
+        {
+            var result = this.adminManager.GetList();
+            return result;
+        }
+    }
+}
