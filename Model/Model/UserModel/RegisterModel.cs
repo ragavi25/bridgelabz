40e0f05dfc7e61ad339e409ef5839b0eeba2c242
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-06-09 15:11:47 +0530

    Update RegisterModel.cs

--- Model/Model/UserModel/RegisterModel.cs
+++ Model/Model/UserModel/RegisterModel.cs
@@ -1,4 +1,4 @@
-﻿// --------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 // <copyright file=AccountController.cs" company="Bridgelabz">
 //   Copyright © 2019 Company="BridgeLabz"
 // </copyright>
@@ -16,11 +16,13 @@ namespace Fundoo.Model
         private string lastName;
         private string email;
         private string password;
+        private string profilePicture;
         public string FirstName { get =>this.firstName; set =>this. firstName = value; }
         public string LastName { get => this.lastName; set => this.lastName = value; }
-        [Key]
         public string Email { get =>this. email; set =>this. email = value; }
         public string Password { get => this.password; set =>this. password = value; }
+        [Key]
         public string Id { get =>this. id; set =>this. id = value; }
+        public string ProfilePicture { get => profilePicture; set => profilePicture = value; }
     }
 }
