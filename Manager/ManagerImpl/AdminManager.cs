f41e6f2552e52dcf2a61dd328104567fe6acdb22
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-06-09 15:16:18 +0530

    [Ragavi]Refactor.Admin

---
+++ Manager/ManagerImpl/AdminManager.cs
@@ -0,0 +1,34 @@
+﻿using Manager.ManagerInterface;
+using Model.Model;
+using Model.Model.Admin;
+using Repository.RepositoryInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.ManagerImpl
+{
+    public class AdminManager : IAdminManager
+    {
+        private readonly IAdmin admin;
+        public AdminManager(IAdmin adminrep)
+        {
+            admin = adminrep;
+        }
+        public Task<int> AddAdmin(Admins admins)
+        {
+            return this.admin.AddAdmin(admins);
+        }
+
+        public List<Adminlist> GetList()
+        {
+            return this.admin.GetList();
+        }
+
+        public string LoginAdmin(Adminlogin admins)
+        {
+            return this.admin.LoginAdmin(admins);
+        }
+    }
+}
