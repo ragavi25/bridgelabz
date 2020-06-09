3de5bbbeb58cf90fbb32a931123a96c5dab820a1
ragavi25 <60532982+ragavi25@users.noreply.github.com>
2020-06-09 15:18:58 +0530

    Add files via upload

---
+++ Repository/RepositoryInterface/IAdmin.cs
@@ -0,0 +1,18 @@
+﻿using Model.Model;
+using Model.Model.Admin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.RepositoryInterface
+{
+   public interface IAdmin
+    {
+        Task<int> AddAdmin(Admins admins);
+       string LoginAdmin(Adminlogin admins);
+
+        List<Adminlist> GetList();
+
+    }
+}
